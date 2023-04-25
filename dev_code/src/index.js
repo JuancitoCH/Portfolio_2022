f406a51a2916497cbac8e0228b4e4fa40c27d49c
CJuan <cjuan.chona@hotmail.com>
2023-04-24 21:38:38 -0300

    Update: card section

--- dev_code/src/index.js
+++ dev_code/src/index.js
@@ -9,7 +9,7 @@ import './static/sass/projects_display.scss'
 
 // animation
 import intersectionob from './js_components_anim/intersectionob'
-import RenderKnowlege from './pages/RenderKnowlege'
+// import RenderKnowlege from './pages/RenderKnowlege'
 import './js_components_anim/splide_mount'
 // helper functions
 import './helpers/copy_clipboard.js'
@@ -24,37 +24,37 @@ const app = document.querySelector('#app')
 intersectionob()
 
 // Routing
-const knowlege_node_list = document.querySelectorAll('.knowlege_card')
-let mousePointer
-let currentColor
-for (const known of knowlege_node_list) {
-    known.addEventListener('click', function (e) {
-        let hash_url
-        for (let node of this.childNodes) {
-            if (node.id = 'name_card') {
-                // hash_url = this.childNodes[1].textContent
-                hash_url = this.querySelector('h3').textContent
-            } else {
-                hash_url = ''
-            }
-        }
-        window.location.hash = hash_url
-        currentColor = window.getComputedStyle(this, null).getPropertyValue('background-color')
+// const knowlege_node_list = document.querySelectorAll('.knowlege_card')
+// let mousePointer
+// let currentColor
+// for (const known of knowlege_node_list) {
+//     known.addEventListener('click', function (e) {
+//         let hash_url
+//         for (let node of this.childNodes) {
+//             if (node.id = 'name_card') {
+//                 // hash_url = this.childNodes[1].textContent
+//                 hash_url = this.querySelector('h3').textContent
+//             } else {
+//                 hash_url = ''
+//             }
+//         }
+//         window.location.hash = hash_url
+//         currentColor = window.getComputedStyle(this, null).getPropertyValue('background-color')
 
-        mousePointer = e
-    })
-}
+//         mousePointer = e
+//     })
+// }
 window.onhashchange = (e) => {
     e.preventDefault()
     const name = window.location.hash
-    const data = {
-        mousePointer,
-        currentColor
-    }
+    // const data = {
+    //     mousePointer,
+    //     currentColor
+    // }
     name != '#' && name!='#_' && 
     name != '#home' && name!='#about'
     && name!='#projects' && name!='#knowledge'
-        && RenderKnowlege(app, name, data)
+        // && RenderKnowlege(app, name, data)
 }
 // window.onload = (e) => {
 //     // window.location.hash = ''
--- dev_code/src/js_components_anim/intersectionob.js
+++ dev_code/src/js_components_anim/intersectionob.js
@@ -1,12 +1,13 @@
 
-const cards_knowledge = document.querySelector('.knowlege').childNodes
+// const cards_knowledge = document.querySelector('.knowlege').childNodes
+const knowledge = document.querySelector('.knowlege')
 const cards_projects = document.querySelector('.projects_v2').childNodes
 const about_section = document.querySelector('#about')
 const observer = new IntersectionObserver((entries,observer)=>{
     entries.forEach(entry => {
         
         if(entry.isIntersecting) {
-            entry.target.classList.remove('io_not_observ')
+            // entry.target.classList.remove('io_not_observ')
             entry.target.classList.add('iobserver')
         }
         // else {
@@ -20,13 +21,27 @@ const observer = new IntersectionObserver((entries,observer)=>{
     // rootMargin: '0px',
     threshold: .3
 })
+const observerRight = new IntersectionObserver((entries,observer)=>{
+    entries.forEach(entry => {
+        
+        if(entry.isIntersecting) {
+            // entry.target.classList.remove('io_not_observ')
+            entry.target.classList.add('iobserver_right')
+        }
+    })
+
+},{
+    // rootMargin: '0px',
+    threshold: .3
+})
 
 export default function io_cards_anim(){
 
-    cards_knowledge.forEach(card=>{
-        if( card.className?.includes('knowlege_card'))
-            observer.observe(card)
-    })
+    // cards_knowledge.forEach(card=>{
+    //     if( card.className?.includes('knowlege_card'))
+    //         observer.observe(card)
+    // })
+    observerRight.observe(knowledge)
     cards_projects.forEach(card=>{
         if( card.className?.includes('one_project_v2'))
             observer.observe(card)
